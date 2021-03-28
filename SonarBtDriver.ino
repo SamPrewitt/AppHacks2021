b1380b60aa4d3cf57d55263c3bfc0906680a9229
Sam Prewitt <prewittsr@appstate.edu>
2021-03-28 13:42:15 -0400

    Update SonarBtDriver.ino

--- SonarBtDriver.ino
+++ SonarBtDriver.ino
@@ -1,3 +1,6 @@
+#include <SoftwareSerial.h>
+SoftwareSerial Blue(0, 1); // 0 is RX pin & 1 is TX pin
+
 //const for Sonar
 const byte DistTrigger_1 = 19; //Pin # on our Teensy
 const byte DistEcho_1 = 18;
@@ -26,28 +29,10 @@ void setup() {
    pinMode(DistEcho_3, INPUT);
 
    // ** Serial2 is for Bluetooth device **
-   Serial2.begin(38400); // Default communication rate of the Bluetooth module 2nd Serial Port
+   //Serial2.begin(38400); // Default communication rate of the Bluetooth module 2nd Serial Port
+   Blue.begin(9600);
    
-   //delay(100);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  float distance_1 = readDistanceSensor(DistTrigger_1, DistEcho_1);
-  float distance_2 = readDistanceSensor(DistTrigger_2, DistEcho_2);
-  float distance_3 = readDistanceSensor(DistTrigger_3, DistEcho_3);
-  
-  Serial.print("Distance 1 sensor reads "); Serial.print(distance_1); Serial.println(" in.");
-  Serial.print("Distance 2 sensor reads "); Serial.print(distance_2); Serial.println(" in.");
-  Serial.print("Distance 3 sensor reads "); Serial.print(distance_3); Serial.println(" in.");
-
-  // Whatever we need to do with bluetooth communication.....
-  if(Serial2.available() > 0) { // Checks whether data is comming from the serial port 2
-     Serial2.read(); // Reads the data from serial port 2
-  }
-  
- 
-  delay(1000);
+   delay(100);
 }
 
 float readDistanceSensor(byte DistTrigger, byte DistEcho) {
@@ -60,3 +45,25 @@ float readDistanceSensor(byte DistTrigger, byte DistEcho) {
    float distance = duration * 0.0343 / 2.0;  // in centimeters
    return (distance * 0.3937); // cm to in
 }
+
+void loop() {
+  // Whatever we need to do with bluetooth communication.....
+  while(Blue.available() == 0);
+  
+  // put your main code here, to run repeatedly:
+  float distance_1 = readDistanceSensor(DistTrigger_1, DistEcho_1);
+  float distance_2 = readDistanceSensor(DistTrigger_2, DistEcho_2);
+  float distance_3 = readDistanceSensor(DistTrigger_3, DistEcho_3);
+  
+  Serial.print("Distance 1 sensor reads "); Serial.print(distance_1); Serial.println(" in.");
+  Blue.write("Distance 1 sensor reads "); Blue.write(distance_1); 
+  
+  Serial.print("Distance 2 sensor reads "); Serial.print(distance_2); Serial.println(" in.");
+  Blue.write("Distance 2 sensor reads "); Blue.write(distance_2); 
+  
+  Serial.print("Distance 3 sensor reads "); Serial.print(distance_3); Serial.println(" in.");
+  Blue.write("Distance 3 sensor reads "); Blue.write(distance_3); 
+
+  delay(3000);
+  }
+  
